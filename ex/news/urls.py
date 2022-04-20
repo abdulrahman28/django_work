a777860e447b2ba3c3bf1f202a615efe8f9b0779
Yusuf Abdulrahman <yusufabdulrahman05@gmail.com>
2022-04-20 10:28:43 +0100

    update

--- ex/news/urls.py
+++ ex/news/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('counter', views.counter, name='counter')
+    path('counter', views.counter, name='counter'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
 ]
--- ex/news/views.py
+++ ex/news/views.py
@@ -1,16 +1,37 @@
-from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 
 def index(request):
+
+
+    features = Feature.objects.all()
+
     context = {
         'name':'Abdul',
         'age':23,
-        'email':'yusuf@gmail.com'
+        'email':'yusuf@gmail.com',
+        'features':features,
     }
     return render(request, 'index.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        passw = request.POST['pass']
+        cpassw = request.POST['cpass']
+        if passw == cpassw:
+            user = request.POST['user']
+    
+
+    return render(request, 'register.html')
+
+
+def login(request):
+
+    return render(request, 'register.html')
+
 
 def counter(request):
     words = request.POST['text']
